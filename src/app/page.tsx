0b9a12201e25aad95e6aeb822627a0c14bee12ab
Ezekiel John Alvar <alvarezekiel19@gmail.com>
2025-12-10 15:01:13 +0800

    Remove Submit Ticket and View Ticket at Home Page. Must be visible only when user is logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,23 +11,8 @@ const HomePage = () => {
             <p className='text-lg text-gray-600 mb-8'>
                 Fast and simple support ticket management system.
             </p>
-
-            <div className='flex flex-col md:flex-row gap-4 justify-center animate-slide opacity-0'>
-                <Link
-                    href='/tickets/new'
-                    className='bg-blue-600 text-white px-6 py-3 rounded shadow hover:bg-blue-700 transition'
-                >
-                    Submit a Ticket
-                </Link>
-                <Link
-                    href='/tickets'
-                    className='bg-blue-100 text-gray-700 px-6 py-3 rounded shadow hover:bg-blue-200 transition'
-                >
-                    View Tickets
-                </Link>
-            </div>
         </main>
-    ); 
+    );
 };
 
 export default HomePage;
