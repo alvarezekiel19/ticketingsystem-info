ceac02e8f99269f3a43c28811f33903153f7ec18
Ezekiel John Alvar <alvarezekiel19@gmail.com>
2025-12-17 14:14:31 +0800

    Added new feature: Use UUID in URL instead of the actual ticket number.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const HomePage = () => {
         <main className='flex flex-col text-center items-center justify-center min-h-screen px-4'>
             <FaTicketAlt className='mx-auto mb-4 text-blue-500' size={60} />
             <h1 className='text-4xl md:text-5xl font-bold mb-4 text-blue-600'>
-                Welcome to Informatics Support Ticket System
+                Welcome to Support Ticket System
             </h1>
             <p className='text-lg text-gray-600 mb-8'>
                 Fast and simple support ticket management system.
